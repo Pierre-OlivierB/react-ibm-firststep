ebfa6d3099e4591362e38f3f9767051b0c8c4eac
unknown <baudinopierreolivier@gmail.com>
2023-03-06 10:45:12 +0100

    add first personal component

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <img src="./logo192.png" width={42} height={42} alt="logo" />
+      <h1 className="title">Projet 1</h1>
+      <ul className="list-group">
+        <li className="list-group-item">Partie 1</li>
+        <li className="list-group-item">Partie 2</li>
+        <li className="list-group-item">Partie 3</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
